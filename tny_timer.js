68dc6feac21cdce8931c8a2765955a8bb521f872
Hunter <hfunk336@west-mec.org>
2020-01-10 14:41:44 -0700

    finished w/ comments

--- tny_timer.js
+++ tny_timer.js
@@ -9,10 +9,38 @@
    Date:   
 
 */
+showClock();
+setInterval("showClock()", 1000);
+// showClock() will run every second
+
 function showClock(){
-   var thisDay = new Date("May 19, 2018 9:31:27");
-   var localDate = thisDay.toDateString();
+   var thisDay = new Date();
+   // var thisDay will be today's date
+   var localDate = thisDay.toLocaleDateString();
+   // changes it to match the local format for date
    var localTime = thisDay.toLocaleTimeString();
+   // changes it to match the local format for time
+
+   document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>";
+   // changes the current time in the HTML to be the localDate and localTime variables
+
+   var j4Date = nextJuly4(thisDay);
+   // j4Date will be what the nextJuly4 returns using the thisDay var
+   // as a parameter
+   j4Date.setHours("21");
+
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   // changes it from miliseconds to days
+   var hrs = (days - Math.floor(days))*24;
+   // changes it from days to hours
+   var mins = (hrs - Math.floor(hrs))*60;
+   // changes it from hours to mins
+   var secs = (mins - Math.floor(mins))*60;
+   // changes it from mins to secs
+   document.getElementById("dLeft").textContent = Math.floor(days);
+   document.getElementById("hLeft").textContent = Math.floor(hrs);
+   document.getElementById("mLeft").textContent = Math.floor(mins);
+   document.getElementById("sLeft").textContent = Math.ceil(secs);
 }
 
 function nextJuly4(currentDate) {
